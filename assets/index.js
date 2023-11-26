44bdf005907f667fec9f6c18c9739bd664164110
Cat <c.z.mai@student.unsw.edu.au>
2023-11-26 13:26:41 +1100

    Added attendee picture placeholders

--- assets/index.js
+++ assets/index.js
@@ -4,3 +4,6 @@ export const calendarIcon = require('./icons/icon-calendar.svg');
 export const mapPinIcon = require('./icons/icon-map-pin.svg');
 export const smileyFaceIcon = require('./icons/icon-smile.svg');
 export const dollarSignIcon = require('./icons/icon-dollar-sign.svg');
+export const imageIcon = require('./icons/icon-image.svg');
+
+export const pdfThumbnail = require('./pdf-thumbnail.svg');
--- components/AttendeesList/AttendeePicture.js
+++ components/AttendeesList/AttendeePicture.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { StyleSheet, Image, View } from "react-native";
 import { COLOURS } from "../../config/styleConstants";
+import { imageIcon } from "../../assets";
 
 export default function AttendeePicture() {
   return (
     <View style={styles.container}>
-      <Image style={styles.attendeePicture}/>
+      <View style={styles.pictureFrame}>
+        <Image source={imageIcon} style={styles.attendeePicture}/>
+      </View>
       <View style={styles.overlap}/>
     </View>
   )
@@ -15,13 +18,20 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
   },
-  attendeePicture: {
+  pictureFrame: {
     height: 43,   // deliberately made these bigger
     width: 43,    // for better visibility of photo
     borderRadius: 40,
     borderWidth: 3,
     borderColor: COLOURS.darkBlue,
     backgroundColor: COLOURS.lightBlue,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  attendeePicture: {
+    height: 24,
+    width: 24,
+    tintColor: COLOURS.mediumBlue,
   },
   overlap: {
     width: 1,
